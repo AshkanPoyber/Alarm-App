1fcdd0b3d8881d12694296105f069c08b0358165
AshkanPoyber <AcerMRD.Pro@gmail.com>
2024-05-16 13:50:03 +0330

    updated

--- AlarmApp.js
+++ AlarmApp.js
@@ -2,5 +2,5 @@ const selectMenu = document.querySelectorAll("select");
 
 for (let i = 12; i > 0; i--) {
   i = i < 10 ? "0" + i : i;
-  let option = `<option value="01">01</option>`;
+  let option = `<option value="${i}">${i}</option>`;
 }
