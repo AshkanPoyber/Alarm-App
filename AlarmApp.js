20a73ab2f658d7d383e1cbd48ea9145dcc6b6d35
AshkanPoyber <AcerMRD.Pro@gmail.com>
2024-05-16 13:54:55 +0330

    updated

--- AlarmApp.js
+++ AlarmApp.js
@@ -12,8 +12,8 @@ for (let i = 59; i >= 0; i--) {
   selectMenu[1].firstElementChild.insertAdjacentHTML("afterend", option);
 }
 
-for (let i = 12; i > 0; i--) {
+for (let i = 2; i > 0; i--) {
   i = i < 10 ? "0" + i : i;
   let option = `<option value="${i}">${i}</option>`;
-  selectMenu[0].firstElementChild.insertAdjacentHTML("afterend", option);
+  selectMenu[2].firstElementChild.insertAdjacentHTML("afterend", option);
 }
